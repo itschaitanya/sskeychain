ee3215c5b41d4c111fe3daf74970c6cfb486367c
Chaitanya Ramavajjala <chaitanyar@zillow.com>
2015-11-01 22:01:56 -0800

    Making Frameworks.

---
+++ SSKeychain Watch/SSKeychain Watch.h
@@ -0,0 +1,19 @@
+//
+//  SSKeychain Watch.h
+//  SSKeychain Watch
+//
+//  Created by Chaitanya Ramavajjala on 10/23/15.
+//  Copyright © 2015 Sam Soffes. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SSKeychain Watch.
+FOUNDATION_EXPORT double SSKeychain_WatchVersionNumber;
+
+//! Project version string for SSKeychain Watch.
+FOUNDATION_EXPORT const unsigned char SSKeychain_WatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSKeychain_Watch/PublicHeader.h>
+
+
